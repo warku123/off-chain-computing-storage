92c2b01ea5ac92efaecf956e7087c77df7ac01c5
zhangpinge <50477615+warku123@users.noreply.github.com>
2023-07-08 15:42:33 +0800

    修改json不能被导入的问题

--- src/data/database.go
+++ src/data/database.go
@@ -3,54 +3,54 @@ package data
 import "errors"
 
 type Data struct {
-	value_hash string `json:"value_hash"`
-	read_num   int    `json:"read_num"`
+	Value_hash string `json:"value_hash"`
+	Read_num   int    `json:"read_num"`
 }
 
 type Data_entry struct {
-	data_tuples []Data `json:"data_tuples"`
-	write_num   int    `json:"write_num"`
+	Data_tuples []Data `json:"data_tuples"`
+	Write_num   int    `json:"write_num"`
 }
 
 type Data_table map[string]Data_entry
 
 func (v *Data_table) AddCid(name, hash string) {
 	if entry, ok := (*v)[name]; ok {
-		entry.data_tuples = append(entry.data_tuples, Data{
-			value_hash: hash,
-			read_num:   0,
+		entry.Data_tuples = append(entry.Data_tuples, Data{
+			Value_hash: hash,
+			Read_num:   0,
 		})
 	} else {
 		(*v)[name] = Data_entry{
-			data_tuples: []Data{
+			Data_tuples: []Data{
 				{
-					value_hash: hash,
-					read_num:   0,
+					Value_hash: hash,
+					Read_num:   0,
 				},
 			},
-			write_num: 0,
+			Write_num: 0,
 		}
 	}
 }
 
 func (v *Data_table) GetCid(name string, version int) string {
-	return (*v)[name].data_tuples[version].value_hash
+	return (*v)[name].Data_tuples[version].Value_hash
 }
 
 func (v *Data_table) AddWriteNum(name string) {
 	if entry, ok := (*v)[name]; ok {
-		entry.write_num++
+		entry.Write_num++
 	} else {
 		(*v)[name] = Data_entry{
-			data_tuples: []Data{},
-			write_num:   1,
+			Data_tuples: []Data{},
+			Write_num:   1,
 		}
 	}
 }
 
 func (v *Data_table) ReduceWriteNum(name string) error {
 	if entry, ok := (*v)[name]; ok {
-		entry.write_num--
+		entry.Write_num--
 	} else {
 		return errors.New("ReduceWriteNum: no such data entry")
 	}
@@ -59,16 +59,16 @@ func (v *Data_table) ReduceWriteNum(name string) error {
 
 func (v *Data_table) GetDataVersionNum(name string) (int, error) {
 	if _, ok := (*v)[name]; ok {
-		return len((*v)[name].data_tuples), nil
+		return len((*v)[name].Data_tuples), nil
 	} else {
 		return 0, errors.New("GetDataVersionNum: no such data entry")
 	}
 }
 
 func (v *Data_table) AddReadNum(name string, version int) {
-	(*v)[name].data_tuples[version].read_num++
+	(*v)[name].Data_tuples[version].Read_num++
 }
 
 func (v *Data_table) ReduceReadNum(name string, version int) {
-	(*v)[name].data_tuples[version].read_num--
+	(*v)[name].Data_tuples[version].Read_num--
 }
